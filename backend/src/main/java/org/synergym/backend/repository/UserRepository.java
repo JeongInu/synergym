48662b89e3d4c6273ce380fd3bbb9b2ea2b2a997
jeongyoon2004@naver.com <jeongyoon2004@naver.com>
2025-05-10 17:50:38 +0900

    feat(users) : service코드 수정 및 createdAt 칼럼삭제

--- backend/src/main/java/org/synergym/backend/repository/UserRepository.java
+++ backend/src/main/java/org/synergym/backend/repository/UserRepository.java
@@ -3,6 +3,6 @@ package org.synergym.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.synergym.backend.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
 }
