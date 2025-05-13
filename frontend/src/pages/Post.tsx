d66be30233201b54e89b4bcff3e71d796ed9ee85
Rosy <rosy.jihye.noh@gmail.com>
2025-05-13 10:51:08 +0900

    feat(post): [create initial post file] for post module

---
+++ frontend/src/pages/Post.tsx
@@ -0,0 +1,77 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+
+
+
+const Post = () => {
+  return (
+    <>
+      <Table>
+        <TableCaption>총 게시글: {Post.length}개</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-16 text-center">번호</TableHead>
+            <TableHead>제목</TableHead>
+            <TableHead className="w-24">작성자</TableHead>
+            <TableHead className="w-24">작성일</TableHead>
+            <TableHead className="w-20 text-center">조회수</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+                <TableRow>
+                    <TableCell className="font-medium">INV001</TableCell>
+                    <TableCell>Paid</TableCell>
+                    <TableCell>Credit Card</TableCell>
+                    <TableCell className="text-right">$250.00</TableCell>
+                </TableRow>
+        </TableBody>
+      </Table>
+
+      
+       <div className="mt-6">
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#" isActive>1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">2</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">3</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
+    </>
+  );
+};
+
+
+export default Post;
